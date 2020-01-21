8080bfb2f4e5a8fe4dd1c401546fd43dc61b1adc
Cesar perozo <cesarmataperozo@gmail.com>
2020-01-21 19:55:21 -0300

    Agg Proyecto

---
+++ build/precache-manifest.3b45837bfb513f329d2521904ab87232.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2109df90459fe82300849fe2e6542bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f3f818355eaeab87153",
+    "url": "/static/css/main.e2e9c96c.chunk.css"
+  },
+  {
+    "revision": "11b8f87e43fa72836a1f",
+    "url": "/static/js/2.267c3ea3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.267c3ea3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f3f818355eaeab87153",
+    "url": "/static/js/main.b8e37997.chunk.js"
+  },
+  {
+    "revision": "ec314a551ed99094ddef",
+    "url": "/static/js/runtime-main.0b20fc42.js"
+  },
+  {
+    "revision": "aa6d54bfa22c49cf58b85efd6f82d010",
+    "url": "/static/media/cancha.aa6d54bf.svg"
+  }
+]);
